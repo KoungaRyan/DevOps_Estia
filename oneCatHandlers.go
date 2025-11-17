83fa311d4cb2cd01a60a9fbefe15b7babe53a119
Kounga <yryanivan6@gmail.com>
2025-11-17 16:15:17 +0100

    Function Get et delete cat implementer

--- oneCatHandlers.go
+++ oneCatHandlers.go
@@ -3,6 +3,28 @@ package main
 import "net/http"
 
 func getCat(req *http.Request) (int, any) {
-	Logger.Info("Cat not found")
-	return http.StatusNotFound, "Cat not found"
+	catId := req.PathValue("catId")
+	Logger.Info("Getting cat", catId)
+
+	if cat , found := catsDatabase[catId]; found {
+		Logger.Info("Cat found")
+		return http.StatusOK, cat
+	} else {
+		Logger.Info("Cat not found")
+		return http.StatusNotFound, "Cat not found"
+	}
+}
+
+func deleteCat(req *http.Request) (int, any) {
+	catId := req.PathValue("catId")
+	Logger.Info("Deleting cat", catId)
+
+	if _, found := catsDatabase[catId]; found {
+		delete(catsDatabase, catId)
+		Logger.Info("Cat deleted")
+		return http.StatusNoContent, catId
+	} else {
+		Logger.Info("Cat not found")
+		return http.StatusNotFound, "Cat not found"
+	}
 }
